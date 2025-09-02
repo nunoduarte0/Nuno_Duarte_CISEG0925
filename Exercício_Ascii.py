4427112016bb4b97d2538140146e1b712314b7f6
Nuno Duarte <nuno.duarte.t0131880@edu.atec.pt>
2025-09-02 20:15:42 +0100

    2

--- Exercício_Ascii.py
+++ Exercício_Ascii.py
@@ -14,3 +14,33 @@
 #Miguel PriMo
 #Luis AnseLmo
 #Guilherme ramos
+
+listname=[]
+
+i=0
+it=0
+
+name=input("Introduza o seu nome")
+
+listname.append(name)
+print(listname)
+
+
+
+while i<len(listname):
+    it=0
+    
+    while it<len(listname[i]):
+        
+      print(listname[i][it])
+      it+=1
+    i+=1
+    
+    
+
+for i in name:
+  char = ord(i)
+  if char >= 65 and char <= 90:
+        print("Maiscula encontrada" , char)
+    
+
